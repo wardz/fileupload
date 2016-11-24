a1ecae831669d7636e1390c361cd12d2eb18ccca
wardz <thomaszrx@gmail.com>
2016-11-24 12:35:07 +0100

    add hyperlinks for cards

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -6,19 +6,21 @@
 
 			@foreach($projects as $project)
 				<div class="col s6">
-					<div class="card small">
-						<div class="card-image">
-							<img src="http://static6.businessinsider.com/image/564372019dd7cc02308bdbfe-538/html-code.jpg">
-						</div>
+					<a href="{!! action('ProjectController@show', $project->name) !!}">
+						<div class="card small">
+							<div class="card-image">
+								<img src="http://static6.businessinsider.com/image/564372019dd7cc02308bdbfe-538/html-code.jpg">
+							</div>
 
-						<div class="card-content">
-							<span class="card-title">{{ $project->name }}</span>
-						</div>
+							<div class="card-content">
+								<span class="card-title">{{ $project->name }}</span>
+							</div>
 
-						<div class="card-action">
-							<span>{{ $project->description }}</span>
+							<div class="card-action">
+								<span>{{ $project->description }}</span>
+							</div>
 						</div>
-					</div>
+					</a>
 				</div>
 			@endforeach
 
