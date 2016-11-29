0fc5073823f993bb6e9a7c849d064a658e997604
wardz <thomaszrx@gmail.com>
2016-11-29 14:42:50 +0100

    add db model factory seeding test

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,7 @@
 <?php
 
+use Storage;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -16,9 +18,57 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name' => $faker->name,
+        'name' => $faker->name->unique(),
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Permission::class, function (Faker\Generator $faker) {
+    return [
+        'role_id' => $faker->numberBetween(0, 3)
+        // user_id
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Tag::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word->unique()
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define('ProjectTag', function (Faker\Generator $faker) {
+    return [
+        'tag_id' => $faker->numberBetween(0, 5)
+        // project_id
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\Project::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->word->unique(),
+        'description' => $faker->paragraph,
+        // user_id
+    ];
+});
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(App\File::class, function (Faker\Generator $faker) {
+	$name = $faker->word . '.zip';
+	$path = Storage::put($name, 'test');
+
+    return [
+        'file_name' => $name,
+        'file_version' => 'v' . $faker->numberBetween(1, 9),
+        'file_changelog' => $faker->paragraph,
+        'file_mime' => 'application/zip',
+        'file_path' => $path,
+        'file_size' => $faker->numberBetween(1024, 10240),
+        // project_id
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,10 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Foundation\Auth\User;
-
-use App\Project;
-use App\File;
-use App\Tag;
+use Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,56 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // TODO create seperate seed class for every model
+        factory(App\Tag::class, 6)->create();
 
-        $user1 = User::create(array(
-            'name' => 'admin',
-            'email' => 'admin@localhost.com',
-            'password' => bcrypt('admin')
-        ));
-
-        $project1 = Project::create(array(
-            'name' => 'Project 1',
-            'description' => 'Project description 10 characters.',
-            'user_id' => $user1->id
-        ));
-
-        //Storage::put('file1.zip', 'content');
-        
-        $file1 = File::create(array(
-            'file_name' => 'file1.zip',
-            'file_version' => 'v1',
-            'file_changelog' => '10 character description',
-            'file_mime' => 'application/zip',
-            'file_path' => 'invalid/path/1.zip',
-            'file_size' => '1234',
-            'project_id' => 1
-        ));
-
-        $user2 = User::create(array(
-            'name' => 'guest',
-            'email' => 'guest@localhost.com',
-            'password' => bcrypt('guest')
-        ));
-
-        $project2 = Project::create(array(
-            'name' => 'Project 2',
-            'description' => 'Project description 10 characters.',
-            'user_id' => $user2->id
-        ));
-
-        $file2 = File::create(array(
-            'file_name' => 'file2.zip',
-            'file_version' => 'v1',
-            'file_changelog' => '10 character description',
-            'file_mime' => 'application/zip',
-            'file_path' => 'invalid/path/2.zip',
-            'file_size' => '10102',
-            'project_id' => 2
-        ));
-
-        Tag::create(array('name' => 'Work'));
-        Tag::create(array('name' => 'Cooking'));
-        Tag::create(array('name' => 'Coding'));
+        factory(App\User::class, 10)->create()->each(function($user) {
+            $user->permissions()->save(factory(App\Permission::class)->make());
+            // permissions()->user()-> ?
+            
+            $user->projects()->save(factory(App\Project::class)->make());
+            $user->projects()->tags()->save(factory('projectTag')->make());
+            $user->projects()->files()->save(factory(App\File::class)->make());
+        });
     }
 }
