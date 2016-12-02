54b2238092e3c1541a8d6ce858873fd430316f70
wardz <thomaszrx@gmail.com>
2016-12-02 23:20:04 +0100

    fix breadcrumb url href

--- resources/views/layouts/breadcrumb.blade.php
+++ resources/views/layouts/breadcrumb.blade.php
@@ -4,8 +4,9 @@
 			<div class="col s12">
 				<a href="{!! url('/') !!}" class="breadcrumb">Home</a>
 
+				<?php $link = url('/'); ?>
 				@for ($i = 1; $i <= count(Request::segments()); $i++)
-					<a href="{{ url(Request::segment($i)) }}" class="breadcrumb">{{ Request::segment($i) }}</a>
+					<a href="{{ url($link .= '/' . Request::segment($i)) }}" class="breadcrumb">{{ Request::segment($i) }}</a>
 				@endfor
 			</div>
 		</div>
